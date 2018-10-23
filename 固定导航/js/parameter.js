9d252d90b40610425cc8a31493fd0772765ec0af
weitao <sk11zwt@126.com>
2018-10-23 19:21:00 +0800

    左侧固定未完成

--- 固定导航/js/parameter.js
+++ 固定导航/js/parameter.js
@@ -1,14 +1,15 @@
 $(function () {
     // 上部导航栏固定
+    var t, top, h, r, s;
+    
     $(this).scroll(function() {
-        var t = $(document).scrollTop();
-        var h = $('table').offset().top;
-        var r = $("#fixed_top").height()
-        var s = $("body").height();
-        console.log(t, r, s, h);
+        t = $(document).scrollTop();
+        top = $('#fixed_top').offset().top - t;
+        $('.left-top-boxs').css('top', top);
+        h = $('table').offset().top;
+        r = $("#fixed_top").height();
+        s = $("body").height();
         s = s - r;
-        console.log(s);
-        
         if (t < h) {
             $("#fixed_top").removeClass('fixed-top');
             $('#fixed_top').next().css('height', 0);
@@ -24,7 +25,38 @@ $(function () {
             $("#fixed_top").removeClass('fiesd-bottom');
         }
     });
+})
+$(function () {
+    var h;
+    ti = $('#fixed_top').offset().top
+    $(this).scroll(function () {
+        var t = $(document).scrollTop();
+        var tl = $(document).scrollLeft()
+        h = $('table').offset().left;
+        console.log(tl, h);
+        var r = $("#fixed_top").height();
+        var w = $("#fixed_top").width();
+        var s = $("body").width();
+        s = s - w;
+        var top = ti + r - t;
+        console.log(top);
+        
+        if (tl < h) {
+            $(".left-top-boxs").addClass('par-fix-left');
+            $('.par-deb-box').addClass('par-fix-left');
+            $('.par-deb-box').css('top', top + 'px')
+        } else {
+           $(".left-top-boxs").removeClass('par-fix-left');
+           $(".par-deb-box").removeClass('par-fix-left');
+        }
+        if (t > s) {
+            $(".left-top-boxs").addClass('par-fix-left');
+            $(".par-deb-box").addClass('par-fix-left');
+            $('.par-deb-box').css('top', top + 'px')
 
-    //  左侧导航栏固定
-    
+        } else {
+            $(".left-top-boxs").removeClass('par-fix-left');
+            $(".par-deb-box").removeClass('par-fix-left');
+        }
+    })
 })
